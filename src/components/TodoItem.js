b828af57fe79a9cc8932a3e77466e7cebfbd112a
LeDucThuan <dthuanle.20@gmail.com>
2019-09-12 21:02:16 +0700

    import check-marks and put event onclick on them; modify css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import App from '../App';
 import './TodoItem.css';
 import classNames from 'classnames';
+import checkImg from '../img/not-success.svg';
+import checkCompleteImg from '../img/success.svg'
 
 class TodoItem extends Component {
     render(){
         const { item, onClick } = this.props;
+        let url = checkImg;
+        if(item.isComplete){
+            url = checkCompleteImg
+        }
         return (
-            <div onClick={onClick} className={classNames("todoItem", { 
+            <div className={classNames("TodoItem", { 
                 "TodoItem-complete": item.isComplete === true
             })} >
+                <img onClick={onClick} src={url} width="32" height="32"></img>
                 <p>{item.title}</p>
             </div>
         );
